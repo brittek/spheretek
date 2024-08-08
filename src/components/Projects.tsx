195c60ee20ce95185cd1def1c77c37c19acc8f17
Brittek <info@brittek.digital>
2024-08-08 15:42:16 +1000

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -35,13 +35,13 @@ export default function Projects({ data, tags }: Props) {
     <div class="grid grid-cols-1 sm:grid-cols-3 gap-6">
       <div class="col-span-3 sm:col-span-1">
         <div class="sticky top-24">
-          <div class="text-sm font-semibold uppercase mb-2 text-black dark:text-white">Filter</div>
+          <div class="text-sm font-semibold uppercase mb-2 text-primary">Filter</div>
           <ul class="flex flex-wrap sm:flex-col gap-1.5">
             <For each={tags}>
               {(tag) => (
                 <li>
-                  <button onClick={() => toggleTag(tag)} class={cn("w-full px-2 py-1 rounded", "whitespace-nowrap overflow-hidden overflow-ellipsis", "flex gap-2 items-center", "bg-black/5 dark:bg-white/10", "hover:bg-black/10 hover:dark:bg-white/15", "transition-colors duration-300 ease-in-out", filter().has(tag) && "text-black dark:text-white")}>
-                    <svg class={cn("size-5 fill-black/50 dark:fill-white/50", "transition-colors duration-300 ease-in-out", filter().has(tag) && "fill-black dark:fill-white")}>
+                  <button onClick={() => toggleTag(tag)} class={cn("w-full px-2 py-1 rounded", "whitespace-nowrap overflow-hidden overflow-ellipsis", "flex gap-2 items-center", "bg-primary/5 dark:bg-secondary/10", "hover:bg-primary/10 hover:dark:bg-secondary/15", "transition-colors duration-300 ease-in-out", filter().has(tag) && "text-primary dark:text-secondary")}>
+                    <svg class={cn("size-5 fill-primary/50 dark:fill-secondary/50", "transition-colors duration-300 ease-in-out", filter().has(tag) && "fill-primary dark:fill-secondary")}>
                       <use href={`/ui.svg#square`} class={cn(!filter().has(tag) ? "block" : "hidden")} />
                       <use href={`/ui.svg#square-check`} class={cn(filter().has(tag) ? "block" : "hidden")} />
                     </svg>
@@ -55,7 +55,7 @@ export default function Projects({ data, tags }: Props) {
       </div>
       <div class="col-span-3 sm:col-span-2">
         <div class="flex flex-col">
-          <div class="text-sm uppercase mb-2">
+          <div class="text-sm uppercase mb-2 text-primary">
             SHOWING {projects().length} OF {data.length} PROJECTS
           </div>
           <ul class="flex flex-col gap-3">
